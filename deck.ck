334a06091394dce21b3a7e5fe70bb88e6d58ffcf
Christoph Helma <helma@in-silico.ch>
2017-06-25 17:24:12 +0200

    stem play separated

--- deck.ck
+++ deck.ck
@@ -2,81 +2,97 @@
 OscSend xmit;
 xmit.setHost( "localhost", 9090);
 
-// stems
-SndBuf2 stems[4]; 
-for (0=>int i; i<4; i++) { 
-  0 => stems[i].play;
-  0 => stems[i].channel;
-  stems[i] => dac.chan(i*2);
-  1 => stems[i].channel;
-  stems[i] => dac.chan(i*2+1);
-}
-
 132.0 => float bpm;
 8 => int ticks_bar;
-0 => int loop;
-0 => int loop_in;
-0 => int loop_out;
 
 fun float tickratio() { return 240/ticks_bar/bpm; }
 fun dur tickdur() { return 1::second*tickratio(); }
 fun float ticksamples() { return 44100*tickratio(); }
-fun int eightbar_samples() { return 8*ticks_bar*ticksamples() $ int; }
 fun int bar_samples() { return ticks_bar*ticksamples() $ int; }
+fun int eightbar_samples() { return 8*ticks_bar*ticksamples() $ int; }
 
-fun float ticks() { return stems[0].pos()/ticksamples(); }
-fun float bars() { return ticks()/ticks_bar; }
-fun float eightbars() { return ticks()/8/ticks_bar; }
+class Stem {
 
-fun int eightbar_offset() { return stems[0].pos() - eightbars()$int * eightbar_samples()$int; }
-fun int next_bar_offset() { return (bars()$int+1)*bar_samples() - stems[0].pos(); }
-fun int bar_offset() { return stems[0].pos() - bars()$int * ticks_bar*ticksamples()$int; }
-fun int tick_offset() { return stems[0].pos() - ticks()$int * ticksamples()$int; }
-
-fun void stop() {
-  for (0=>int i; i<4; i++) {
-    0 => stems[i].play;
-    0 => stems[i].pos;
-  }
+  SndBuf2 buf;
   0 => int loop;
   0 => int loop_in;
   0 => int loop_out;
-}
+  0 => buf.play;
+  int nr;
 
-fun void seek(int samples) {
-  for (0=>int i; i<4; i++) { 
-    samples => stems[i].pos;
-    1 => stems[i].play;
+  fun void connect(int i) {
+    i => nr;
+    buf => dac;
+    //0 => buf.channel;
+    //buf => dac.chan(nr*2);
+    //1 => buf.channel;
+    //buf => dac.chan(nr*2+1);
   }
-}
 
-fun void rate(float r) { for (0=>int i; i<4; i++) { r => stems[i].rate; } }
-
-fun void position() {
-  while (true) {
-    if (stems[0].play() == 1) {
-      xmit.startMsg( "/phase", "f" );
-      stems[0].phase() => xmit.addFloat;
-    }
-    30::ms => now; // ~ 30fps
+  fun void read(string file) {
+    stop();
+    file => buf.read;
   }
-}
 
-fun void looper() {
-  while (true) {
-    if (loop == 1 && loop_out > loop_in) {
-      if (stems[0].pos() == loop_out) {
-        for (0=>int i; i<4; i++) { loop_in => stems[i].pos; }
+  fun void stop() {
+    0 => buf.play;
+    0 => buf.pos;
+    0 => int loop;
+    0 => int loop_in;
+    0 => int loop_out;
+  }
+
+  fun void seek(int samples) {
+      samples => buf.pos;
+      1 => buf.play;
+  }
+
+  fun void nextbar(int eightbar) {
+    next_bar_offset()::samp => now;
+    seek(eightbar*eightbar_samples());
+  }
+
+  fun void rate(float r) { r => buf.rate; }
+
+  fun void position() {
+    while (true) {
+      if (buf.play() == 1) {
+        xmit.startMsg( "/phase", "if" );
+        nr => xmit.addInt;
+        buf.phase() => xmit.addFloat;
       }
+      30::ms => now; // ~ 30fps
     }
-    1::samp => now;
   }
+
+  fun void looper() {
+    while (true) {
+      if (loop == 1 && loop_out > loop_in) {
+        if (buf.pos() == loop_out) { loop_in => buf.pos; }
+      }
+      1::samp => now;
+    }
+  }
+
+  fun float ticks() { return buf.pos()/ticksamples(); }
+  fun float bars() { return ticks()/ticks_bar; }
+  fun float eightbars() { return ticks()/8/ticks_bar; }
+
+  fun int eightbar_offset() { return buf.pos() - eightbars()$int * eightbar_samples(); }
+  fun int next_bar_offset() { return (bars()$int+1)*bar_samples() - buf.pos(); }
+  fun int bar_offset() { return buf.pos() - bars()$int * ticks_bar*ticksamples()$int; }
+  fun int tick_offset() { return buf.pos() - ticks()$int * ticksamples()$int; }
 }
 
-fun void nextbar(int eightbar) {
-  next_bar_offset()*1::samp => now;
-  seek(eightbar*eightbar_samples());
-}
+// stems
+Stem stems[4]; 
+for (0=>int i; i<4; i++) { stems[i].connect(i); }
+
+fun void stop() { for (0=>int i; i<4; i++) { stems[i].stop(); } }
+
+fun void seek(int samples) { for (0=>int i; i<4; i++) { stems[i].seek(samples); } }
+
+fun void rate(float r) { for (0=>int i; i<4; i++) { stems[i].rate(r); } }
 
 fun void controller() {
 
@@ -89,28 +105,30 @@ fun void controller() {
   while(true) {
     oin => now;
     while(oin.recv(msg)) {
-      if (msg.address == "/load") {
-        stop();
-        msg.getString(0) => string dir;
-        for (0=>int s; s<4; s++) {
-          dir + "/" + s + ".wav" => string file;
-          file => stems[s].read;
-        }
+      if (msg.address == "/read") {
+        stems[msg.getInt(0)].read(msg.getString(1));
       }
       else if (msg.address == "/goto/8bar/quant") {
-        seek(msg.getInt(0)*eightbar_samples()+eightbar_offset());
+        msg.getInt(0) => int i;
+        stems[i].seek(msg.getInt(1)*eightbar_samples()+stems[i].eightbar_offset());
       }
-      else if (msg.address == "/goto/8bar/nextbar") {
-      spork ~ nextbar(msg.getInt(0));
+      else if (msg.address == "/goto/8bar/next") {
+        msg.getInt(0) => int i;
+        spork ~ stems[i].nextbar(msg.getInt(1));
       }
       else if (msg.address == "/goto/8bar/now") {
-        seek(msg.getInt(0)*eightbar_samples());
+        msg.getInt(0) => int i;
+        stems[i].seek(msg.getInt(0)*eightbar_samples());
       }
-      else if (msg.address == "/loop/on") { 1 => loop; }
-      else if (msg.address == "/loop/off") { 0 => loop; }
+      else if (msg.address == "/loop/on") { for (0=>int i; i<4; i++) { 1 => stems[i].loop; } }
+      else if (msg.address == "/loop/off") { for (0=>int i; i<4; i++) { 0 => stems[i].loop; } }
       else if (msg.address == "/loop/set/8bar") {
-        8*ticks_bar*ticksamples()$int*msg.getInt(0) => loop_in;
-        8*ticks_bar*ticksamples()$int*msg.getInt(1) => loop_out;
+        for (0=>int i; i<4; i++) {
+          8*ticks_bar*ticksamples()$int*msg.getInt(0) => stems[i].loop_in;
+        }
+        for (0=>int i; i<4; i++) {
+          8*ticks_bar*ticksamples()$int*msg.getInt(1) => stems[i].loop_out;
+        }
       }
       else if (msg.address == "/speed/up") { rate(1.02); }
       else if (msg.address == "/speed/down") { rate(0.98); }
@@ -120,6 +138,9 @@ fun void controller() {
   }
 }
 
-spork ~ position();
-spork ~ looper();
+for (0=>int i; i<4; i++) {
+  spork ~ stems[i].position();
+  spork ~ stems[i].looper();
+}
+
 controller();
--- display.rb
+++ display.rb
@@ -4,104 +4,161 @@ require 'ruby-osc'
 require 'ruby2d'
 
 include OSC
-@client = Client.new 9091
+@@client = Client.new 9091
 
-bpm = 132.0
-w = 1920
-h = 1080
-set width: w
-set height: h
+BPM = 132.0
+EIGHTBAR_SAMPLES = 44100 * 8 * 8 * 30/BPM
 
-stems = Array.new(4);
-samples = 0
-eightbar_samples = 44100 * 8 * 8 * 30/bpm
-grid = []
-gridnr = 0
+WIDTH = 1920
+HEIGHT = 1080
+set width: WIDTH
+set height: HEIGHT
 
-@looprange = Rectangle.new(0, 0, 0, h, [0.3,0.3,0.3,0.5])
-cursor = Rectangle.new(0, 0, 0, h, "green")
-slice = Rectangle.new(0, 0, w*eightbar_samples/samples, h, [0.2,0.2,0.2,0.2])
+class Stem
 
-looping = false
-select_loop = false
-loop_in = 0
-loop_out = 0
+  attr_reader :samples
 
-def toggle_loop
-  looping = !looping
-  looping ? @client.send(Message.new('/loop/on')) : @client.send(Message.new('/loop/off'))
-  looping ? @looprange.color = [0.5,0.2,0.2,0.5] : @looprange.color = [0.3,0.3,0.3,0.5]
+  def initialize wav, nr
+    @file = wav
+    @nr = nr
+    @@client.send(Message.new("/read",@nr,@file))
+    img = wav.sub('wav','png')
+    unless (File.exists?(img) and FileUtils.uptodate?(img,[wav]))
+      `ffmpeg -y -i "#{wav}" -filter_complex 'showwavespic=s=#{WIDTH}x#{height}:colors=white[a];color=s=#{WIDTH}x#{height}:color=black[b];[b][a]overlay'  -frames:v 1 "#{img}"`
+    end
+    @image = Image.new(0,y,img)
+    @image.width = WIDTH
+    @image.height = HEIGHT/4
+    @samples = `soxi "#{@file}" |grep Duration|cut -d '=' -f2|sed  's/samples//'|tr -d " "`.to_i
+    @phase = Rectangle.new(0, y, 1, height, "green")
+    @cursor = Rectangle.new(0, y, WIDTH*EIGHTBAR_SAMPLES/@samples, height, [0.5,0.5,0.5,0.5])
+    @loop = Rectangle.new(0, y, 0, height, [0.3,0.3,0.3,0.5])
+    @select_loop = false
+  end
+
+  def y
+    @nr*HEIGHT/4
+  end
+
+  def height
+    HEIGHT/4
+  end
+
+  def toggle_loop
+    @@client.send(Message.new("/loop/toggle",@nr))
+  end
+
+  def goto_8bar_quant gridnr
+    @@client.send Message.new('/goto/8bar/quant', @nr, gridnr)
+  end
+  
+  def goto_8bar_now gridnr
+    @@client.send Message.new('/goto/8bar/now', @nr, gridnr)
+  end
+  
+  def goto_8bar_next gridnr
+    @@client.send Message.new('/goto/8bar/next', @nr, gridnr)
+  end
+
+  def phase= s
+    @phase.x = s*WIDTH
+  end
+
+  def loop= status
+    status == 1 ? @loop.color = [0.5,0.2,0.2,0.5] : @loop.color = [0.3,0.3,0.3,0.5]
+  end
+
+  def loop_in= start
+    @select_loop = true
+    @loop.x = start
+  end
+
+  def loop_out= out
+    @@client.send Message.new('/loop/set/8bar', loop_in, loop_out)
+    @select_loop = false
+  end
+
+  def cursor= s
+    @cursor.x = s
+  end
+
+  def cursor_off
+    @cursor.color = [0,0,0,0]
+  end
+
+  def cursor_on
+    #@cursor.width = WIDTH*EIGHTBAR_SAMPLES/@samples
+    @cursor.color = [0.5,0.5,0.5,0.5]
+  end
+
 end
 
+singlestem = false
+stems = []
+grid = []
+selected_stems = []
+gridnr = 0
+
 on :mouse_move do |event|
+  singlestem ? selected_stems = [stems[(4*event.y/HEIGHT).floor]] : selected_stems =  stems
   gridnr = grid.index( grid.select{|g| event.x < g}.first)
   gridnr = grid.size unless gridnr
   gridnr -= 1
-  select_loop ? @looprange.width = grid[gridnr+1]-grid[loop_in] : slice.x = grid[gridnr]
+  selected_stems.each {|s| s.cursor = grid[gridnr]}
+  stems.each{|s| s.cursor_off if s} if singlestem
+  selected_stems.each{|s| s.cursor_on if s}
 end
 
 on :mouse_down do |event|
-  if select_loop
-  else
-    case event.button
-    when :left
-      @client.send Message.new('/goto/8bar/quant', gridnr)
-    when :right
-      @client.send Message.new('/goto/8bar/now', gridnr)
-    end
+  case event.button
+  when :left
+    selected_stems.each {|s| s.goto_8bar_quant gridnr }
+  when :right
+    selected_stems.each {|s| s.goto_8bar_now gridnr }
   end
 end
 
 on :key_down do |event|
   case event.key
   when 'escape'
-    @client.send Message.new('/stop')
+    @@client.send Message.new('/stop')
   when 'q'
-    @client.send Message.new('/stop')
+    @@client.send Message.new('/stop')
     close
   when 'a'
     dir = `ls -d ~/music/live/dj/* | dmenu -l 20`.chomp
-    @client.send Message.new('/load', dir)
     clear
-    Dir["#{dir}/[0-3].wav"].each_with_index do |st,i|
-      if File.exists?(st)
-        img = st.sub('wav','png')
-        unless (File.exists?(img) and FileUtils.uptodate?(img,[st]))
-          `ffmpeg -y -i "#{st}" -filter_complex 'showwavespic=s=#{w}x#{h/4}:colors=white[a];color=s=#{w}x#{h/4}:color=black[b];[b][a]overlay'  -frames:v 1 "#{img}"`
-        end
-        stems[i] = Image.new(0,i*h/4,img)
-        stems[i].width = w
-        stems[i].height = h/4
+    Dir["#{dir}/[0-3].wav"].each_with_index do |wav,i|
+      if File.exists?(wav)
+        @@client.send Message.new('/load', dir)
+        stems << Stem.new(wav, i)
       end
     end
-    samples = `soxi "#{dir}/0.wav" |grep Duration|cut -d '=' -f2|sed  's/samples//'|tr -d " "`.to_i
+    samples = stems.collect {|s| s.samples}.max
     pos = 0
     grid = []
-    while (pos <= w) do
+    while (pos <= WIDTH) do
       grid << pos
-      grid.size % 4 == 0 ? Rectangle.new(pos, 0, 1, h, [1,1,1,0.5]) : Rectangle.new(pos, 0, 1, h, [0.5,0.5,0.5,0.5])
-      pos += w*eightbar_samples/samples
+      grid.size % 4 == 0 ? Rectangle.new(pos, 0, 1, HEIGHT, [1,1,1,0.5]) : Rectangle.new(pos, 0, 1, HEIGHT, [0.5,0.5,0.5,0.5])
+      pos += WIDTH*EIGHTBAR_SAMPLES/samples
     end
     gridnr = 0
     txt = Text.new(4, 0, dir.split('/').last, 20, 'vera.ttf')
-    slice = Rectangle.new(0, 0, w*eightbar_samples/samples, h, [0.5,0.5,0.5,0.5])
-    cursor = Rectangle.new(0, 0, 1, h, "green")
-    @looprange = Rectangle.new(0, 0, 0, h, [0.3,0.3,0.3,0.5])
   when '/'
-    toggle_loop
+    selected_stems.each {|s| s.toggle_loop }
   when "left shift"
-    loop_in = gridnr
-    @looprange.x = grid[loop_in]
-    @looprange.width = grid[1]-grid[0]
-    select_loop = true
-  when "backspace"
-    @client.send Message.new('/stop')
+    selected_stems.each {|s| s.loop_in = grid[gridnr] }
+  when "left ctrl"
+    stems.each{|s| s.cursor_off if s}
+    singlestem = true
+    #p selected_stems.size
+    selected_stems.each{|s| s.cursor_on if s}
   when 'space'
-    @client.send Message.new('/goto/8bar/nextbar', gridnr)
+    selected_stems.each {|s| s.goto_8bar_next gridnr}
   when 'right'
-    @client.send Message.new('/speed/up')
+    @@client.send Message.new('/speed/up')
   when 'left'
-    @client.send Message.new('/speed/down')
+    @@client.send Message.new('/speed/down')
   else
     p event.key
   end
@@ -110,14 +167,14 @@ end
 on :key_up do |event|
   case event.key
   when "left shift"
-    loop_out = gridnr + 1
-    @client.send Message.new('/loop/set/8bar', loop_in, loop_out)
-    select_loop = false
-    toggle_loop unless looping
+    selected_stems.each {|s| s.loop_out = grid[gridnr]+1 }
+  when "left ctrl"
+    singlestem = false
+    #stems.each{|s| s.cursor_on if s}
   when 'right'
-    @client.send Message.new('/speed/normal')
+    @@client.send Message.new('/speed/normal')
   when 'left'
-    @client.send Message.new('/speed/normal')
+    @@client.send Message.new('/speed/normal')
   end
 end
 
@@ -125,7 +182,13 @@ thr = Thread.new do
   OSC.run do
     server = Server.new 9090
     server.add_pattern "/phase" do |*args|
-      cursor.x = args[1]*w
+      stems[args[1]].phase = args[2] if stems[args[1]]
+    end
+    server.add_pattern "/loop" do |*args|
+      stems[args[1]].loop args[2]
+    end
+    server.add_pattern "/loop/start" do |*args|
+      stems[args[1]].loopstart args[2]
     end
   end
 end
